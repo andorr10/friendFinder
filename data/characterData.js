e25657e47298abc7cb82d7633fa3842d12afa4b2
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2017-11-10 15:57:28 -0600

    connected front and back end

---
+++ data/characterData.js
@@ -0,0 +1,90 @@
+var got_chars = [
+  {
+    routeName: "Hodor",
+    name: "Hodor",
+    team: "Stark",
+    age: 40,
+    score: 10
+  },
+  {
+    routeName: "Bran",
+    name: "Bran",
+    team: "Stark",
+    age: 17,
+    score: 20
+  },
+  {
+    routeName: "Snow",
+    name: "John Snow",
+    team: "Stark",
+    age: 20,
+    score: 30
+  },
+  {
+    routeName: "Tyrion",
+    name: "Tyrion Lanister",
+    team: "Lanister",
+    age: 34,
+    score: 10
+  },
+  {
+    routeName: "Tywin",
+    name: "Tywin Lanister",
+    team: "Lanister",
+    age: 60,
+    score: 20
+  },
+  {
+    routeName: "Cersei",
+    name: "Cersei Lanister",
+    Team: "Lanister",
+    age: 39,
+    score: 30
+  },
+  {
+    routeName: "Bronn",
+    name: "Bronn",
+    Team: "Power",
+    age: 35,
+    score: 10
+  },
+  {
+    routeName: "Little_Finger",
+    name: "Little Finger",
+    team: "Power",
+    age: 45,
+    score: 20
+  },
+  {
+    routeName: "knight_king",
+    name: "The Knight King",
+    team: "Power",
+    age: 500,
+    score: 30
+  },
+  {
+    routeName: "Rhaegar",
+    name: "Rhaegar Targaryen",
+    team: "Targaryen",
+    age: 21,
+    forcePoints: 100
+  },
+  {
+    routeName: "Danaerys",
+    name: "Danaerys Targaryen",
+    team: "Targaryen",
+    age: 32,
+    forcePoints: 20
+  },
+  {
+    routeName: "Aerys",
+    name: "Aerys Targaryen",
+    role: "Targaryen",
+    age: 55,
+    forcePoints: 30
+  }
+
+];
+
+
+module.exports = got_chars;
---
+++ data/friendData.js
@@ -0,0 +1,18 @@
+var friendData = [
+  {
+    name: "Jim",
+    character: "Little Finger",
+    team: "Power",
+    photo: "image.png",
+    scores: 30
+  },
+  {
+    name: "Jenny",
+    character: "Hodor",
+    team: "Stark",
+    photo: "image2.png",
+    scores: 10
+  }
+];
+
+module.exports = friendData;
